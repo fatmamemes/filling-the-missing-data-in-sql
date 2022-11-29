0be55661bedae76c767d6cb2a5e1be7324733b3e
FATMAMEMES <36711570+fatmamemes@users.noreply.github.com>
2022-11-29 04:18:28 +0300

    Delete replacetomedian.sql

--- replacetomedian.sql
+++
@@ -1,6 +0,0 @@
-select country,date, daily_vaccinations  from(select country,date,daily_vaccinations, row_number() over(partition by country order by daily_vaccinations) as      
-         MEDIAN,count(*) over(partition by country) as cnt
-  from country_vaccination_stats) x where MEDIAN in (FLOOR(cnt/2), cnt/2 + 1);
-  UPDATE [country_vaccination_stats]
-  SET [daily_vaccinations]=0
-  WHERE [daily_vaccinations] IS NULL;
